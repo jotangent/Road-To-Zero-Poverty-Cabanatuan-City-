e2fd7ac83217f64c0931ad2e889fc4363044674a
jotangent <jsdelacruz013@gmail.com>
2022-08-06 17:47:20 +0800

    adding files

---
+++ main.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import joblib
+
+from lime.lime_text import LimeTextExplainer
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from lime import lime_text
+from matplotlib import pyplot as plt
+
+st.set_page_config(layout="wide")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+add_selectbox = st.sidebar.selectbox(
+    "Topics",
+    ("Introduction", "EDAs", "Poverty Classifier at Household Level", "Poverty Rate at Barangay Level", "Clustered Barangay")
+)
+
+loaded_model = joblib.load('lr_model.sav')
+ml_reg = joblib.load('ml_reg.sav')
+classification = joblib.load('classification.sav')
+scaler = joblib.load('scaler.sav')
+
+class_names = {"Poor","Not Poor"}
+
+if add_selectbox == "Introduction":
+    print("Introduction")    
+
+elif add_selectbox == "EDAs"
+    print("EDA")
+
+elif add_selectbox == "Poverty Classifier at Household Level":
+    st.header("Check if a household is poor using input values")
+    st.text("Please")
+
+    ndeath05 = st.number_input(label="number of children who died ages 0-5", step=1)
+    nmaln05 = st.number_input(label="number of children who experienced malnutrition ages 0-5", step=1)
+    msh = st.text_input(label)
+
+    # if st.button("Predict"):
+
+
+
+
+        
+
+
+
+
+
+        
+
+
+    
+
+        
