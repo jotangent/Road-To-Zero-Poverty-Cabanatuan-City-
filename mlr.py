829f8403a8286974faedf3bedda75562b297beb9
gelomaandal <gelomaandal@gmail.com>
2022-08-21 22:20:14 +0800

    added code for recommender engine in MLR using Euclidean Distance

--- mlr.py
+++ mlr.py
@@ -16,6 +16,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances, cosine_similarity
 
 
 
@@ -25,7 +26,7 @@ shapefile["x"] = shapefile.geometry.centroid.x
 shapefile["y"] = shapefile.geometry.centroid.y
 
 
-### ---------------------------------------------------------------
+### Renaming of shapfile column names to match the prop_cols columns---------------------------------------------------------------
 shapefile = shapefile.rename(columns = {'Infant Mor': 'Infant Mortality Deaths',
                           'Malnourish': 'Malnourished Children 5 Yrs. Below', 
                           'Living as': 'Living as Squatters',    
@@ -81,7 +82,7 @@ if my_page == 'Multiple Linear Regression':
     st.title("Input Desired Percentage Value in Brgy. Level")
     st.caption("Values are automatically in % unit. No need to type %.")
     
-# testing only for df and shp file appearance in streamlit
+# #testing only for df and shp file appearance in streamlit
 #     st.title(prop_cols.columns) #this is a df
 #     st.title(shapefile.columns) #this is a shp file
 
@@ -91,12 +92,12 @@ if my_page == 'Multiple Linear Regression':
     
     col1, col2 = st.columns([1.2,1.2])
     with col1:
-        ndeath05_desc = '<p style="font-family:Arial; color:White; font-size: 20px;"><b>Infant Mortality Ages 0-5</b></p>'
+        ndeath05_desc = '<p style="font-family:Arial; font-size: 20px;"><b>Infant Mortality Ages 0-5</b></p>'
         st.markdown(ndeath05_desc, unsafe_allow_html=True) 
         st.caption("Proportion of Infant Mortality =  (No. of Infant Death Ages 0-5)  /  (Total Brgy. Population of Infant Ages 0-5)")
         ndeath05 = st.number_input('Max Value is 2.02%', min_value=0.0, max_value=2.02)
     with col2:
-        nntsws_desc = '<p style="font-family:Arial; color:White; font-size: 20px;"><b>People with No Access to Safe Water</b></p>'
+        nntsws_desc = '<p style="font-family:Arial; font-size: 20px;"><b>People with No Access to Safe Water</b></p>'
         st.markdown(nntsws_desc, unsafe_allow_html=True)
         st.caption("Proportion of No Access to Safe Water =  (No. of People with No Access to Safe Water)  /  (Total Brgy. Population)")
         nntsws = st.number_input('Max Value is 9.34%', min_value=0.0, max_value=9.34)
@@ -107,14 +108,17 @@ if my_page == 'Multiple Linear Regression':
     
     col1, col2 = st.columns([1.2,1.2])
     with col1:
-        ntert1721_desc = '<p style="font-family:Arial; color:White; font-size: 20px;"><b>People that are Not on Tertiary Ages 17-21</b></p>'
+        ntert1721_desc = '<p style="font-family:Arial; font-size: 20px;"><b>People that \
+        are Not on Tertiary Ages 17-21</b></p>'
         st.markdown(ntert1721_desc, unsafe_allow_html=True)
-        st.caption("Proportion of Ages 17-21 Not on Tertiary =  (No. of Ages 17-21 Not on Tertiary)  /  (Total Brgy. Population of Ages 17-21)")
+        st.caption("Proportion of Ages 17-21 Not on Tertiary =  (No. of Ages 17-21 Not on Tertiary)  \
+        /  (Total Brgy. Population of Ages 17-21)")
         ntert1721 = st.number_input('Max Value is 78.43%', min_value=0.0, max_value=78.43)
     with col2:
-        nntliter10ab_desc = '<p style="font-family:Arial; color:White; font-size: 20px;"><b>People that are Not Literate</b></p>'
+        nntliter10ab_desc = '<p style="font-family:Arial; font-size: 20px;"><b>People that are Not Literate</b></p>'
         st.markdown(nntliter10ab_desc, unsafe_allow_html=True)
-        st.caption("Proportion of Illiterate Ages 10 above =  (No. of Illiterate Ages 10 above)  /  (Total Brgy. Population of Ages 10 above)")
+        st.caption("Proportion of Illiterate Ages 10 above =  (No. of Illiterate Ages 10 above)  \
+        /  (Total Brgy. Population of Ages 10 above)")
         nntliter10ab = st.number_input('Max Value is 12.58%', min_value=0.0, max_value=12.58)
     
     
@@ -123,12 +127,12 @@ if my_page == 'Multiple Linear Regression':
     
     col1, col2 = st.columns([1.2,1.2])
     with col1:
-        nfshort_desc = '<p style="font-family:Arial; color:White; font-size: 20px;"><b>People that Experienced Food Shortage</b></p>'
+        nfshort_desc = '<p style="font-family:Arial; font-size: 20px;"><b>People that Experienced Food Shortage</b></p>'
         st.markdown(nfshort_desc, unsafe_allow_html=True)
         st.caption("Proportion of Experienced Food Shortage =  (No. of People Experienced Food Shortage)  /  (Total Brgy. Population)")
         nfshort = st.number_input('Max Value is 7.24', min_value=0.0, max_value=7.24)
     with col2:
-        nvictcr_desc = '<p style="font-family:Arial; color:White; font-size: 20px;"><b>Victims of Crime</b></p>'
+        nvictcr_desc = '<p style="font-family:Arial; font-size: 20px;"><b>Victims of Crime</b></p>'
         st.markdown(nvictcr_desc, unsafe_allow_html=True)
         st.caption("Proportion of Victims of Crime =  (No. of Victims of Crime)  /  (Total Brgy. Population)")
         nvictcr = st.number_input('Max Value is 2.2', min_value=0.0, max_value=2.2)
@@ -139,15 +143,17 @@ if my_page == 'Multiple Linear Regression':
     
     col1, col2 = st.columns([1.2,1.2])
     with col1:
-        age_dep_desc = '<p style="font-family:Arial; color:White; font-size: 20px;"><b>Dependents</b></p>'
+        age_dep_desc = '<p style="font-family:Arial; font-size: 20px;"><b>Dependents</b></p>'
         st.markdown(age_dep_desc, unsafe_allow_html=True)
-        st.caption("Proportion of Dependents =  (No. of Dependent Ages 10-14) + (No. of Dependent Ages 65 above)  /  (Total Brgy. Population)")
+        st.caption("Proportion of Dependents =  (No. of Dependent Ages 10-14) + (No. of Dependent Ages 65 above)  \
+        /  (Total Brgy. Population)")
         st.caption("")
         age_dep = st.number_input('Max Value is 44.10', min_value=0.0, max_value=44.10)
     with col2:
-        dep_desc = '<p style="font-family:Arial; color:White; font-size: 20px;"><b>Unemployed</b></p>'
+        dep_desc = '<p style="font-family:Arial; font-size: 20px;"><b>Unemployed</b></p>'
         st.markdown(dep_desc, unsafe_allow_html=True)
-        st.caption("Proportion of Unemployed =  [(Total Brgy. Population) - (Total No. of Labor Force) - (Total No. of Unemployed 15 above)]  /  (Total Brgy. Population)")
+        st.caption("Proportion of Unemployed =  [(Total Brgy. Population) - (Total No. of Labor Force) - \
+        (Total No. of Unemployed 15 above)]  /  (Total Brgy. Population)")
         dep = st.number_input('Max Value is 84.84', min_value=0.0, max_value=84.84)
         
     
@@ -171,6 +177,7 @@ if my_page == 'Multiple Linear Regression':
                 st.caption("")
                 st.subheader("Predicted Percentage of Poor in Barangay is: ")
             with col2:
+                result_mlr_final = 0.00
                 st.title('0.00%')
             with col3:
                 st.write("")
@@ -183,10 +190,73 @@ if my_page == 'Multiple Linear Regression':
                 st.caption("")
                 st.subheader("Predicted Percentage of Poor in Barangay is: ")
             with col2:
-                st.title(str(("{:.2f}".format(float(result_mlr)))) + '%')
+                result_mlr_final = float(result_mlr)
+                st.title(str(("{:.2f}".format(result_mlr_final))) + '%')
             with col3:
                 st.write("")
-            
+
+### Recommender engine for Barangay using Cosine Similarity
+        #Create a df for to_predict list
+        to_predict_dict = {
+            'Infant Mortality Deaths' : [ndeath05],
+            'No Access to Safe Water' : [nntsws],
+            'Ages (17-21) Not in Tertiary' : [ntert1721],
+            'Ages (10 and above) Not Literate' : [nntliter10ab],
+            'Experienced Food Shortage' : [nfshort],
+            'Number of Victims of Crime' : [nvictcr],
+            'Dependents  Ages (0-14, 65+)' : [age_dep],
+            'Unemployed Dependents' : [dep],
+            'Poor Household' : [result_mlr_final]
+        }
+
+        to_predict_df = pd.DataFrame(to_predict_dict)
+        
+#         st.dataframe(to_predict_df)
+
+        
+        feature_cols = ['Poor Household', 'Infant Mortality Deaths', 'No Access to Safe Water', 'Ages (17-21) Not in Tertiary', 
+                        'Ages (10 and above) Not Literate', 'Experienced Food Shortage', 'Number of Victims of Crime', 
+                        'Dependents  Ages (0-14, 65+)', 'Unemployed Dependents']
+        
+        prop_cols['Euclidean Distance'] = prop_cols.apply(lambda x: euclidean_distances(x[feature_cols].values.reshape(-1, 1),\
+                                                                  to_predict_df[feature_cols].values.reshape(-1, 1))\
+                                                                  .flatten()[0], axis=1)
+        
+        recommendation_df = prop_cols.sort_values('Euclidean Distance')[:3]
+        
+        
+        st.caption("Below are the most similar barangays compared to the Predicted Percentage of Poor in Barangay and the input values.")
+        st.caption("The closer the cosine distance to zero means it has high similarity.")
+        st.caption("")
+        st.dataframe(recommendation_df[['barangay', 'Euclidean Distance'] + feature_cols])
+        
+        
+        
+                
+#                 [ndeath05, nntsws, ntert1721, nntliter10ab, nfshort, nvictcr, age_dep, dep]
+
+#                 shapefile = shapefile.rename(columns = {'Infant Mor': 'Infant Mortality Deaths',
+#                           'Malnourish': 'Malnourished Children 5 Yrs. Below', 
+#                           'Living as': 'Living as Squatters',    
+#                           'Living in': 'Living in Makeshift Housing',
+#                           'No Access': 'No Access to Sanitary Toilet',
+#                           'No Acces_1':'No Access to Safe Water',
+#                           'Ages (5 an': 'Ages (5 and below) Not in Kinder',           
+#                           'Ages (6-11': 'Ages (6-11) Not in Elementary',    
+#                           'Ages (12-1': 'Ages (12-15) Not in Junior High School',
+#                           #'nntelem612_prop': 'Ages (6-12) Not Attending Elementary', 
+#                           #'nnths1316_prop': 'Ages (13-16) Not Attending Secondary',
+#                           'Ages (16-1': 'Ages (16-17) Not Senior High School',                              
+#                           'Ages (17-2': 'Ages (17-21) Not in Tertiary',
+#                           'Ages (10 a': 'Ages (10 and above) Not Literate',
+#                           'Poor House': 'Poor Household',    
+#                           'Subsistent': 'Subsistently Poor Household',                
+#                           'Experience': 'Experienced Food Shortage',                
+#                           'Ages (15 a': 'Ages (15 and Above) Unemployed',
+#                           'Number of':'Number of Victims of Crime',   
+#                           'Dependents': 'Dependents  Ages (0-14, 65+)',
+#                           'Unemployed': 'Unemployed Dependents',
+#                           })            
            
         
 # #         result_mlr_desc = '<p style="font-family:Arial; color:White; font-size: 20px;">Predicted Percentage of Poor Household in a Barangay is:</p>'   
